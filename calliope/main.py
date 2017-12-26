5dc134d049144516f2d3d580bcb05bebf520f2f4
K <kwailamchan@hotmail.com>
2017-12-27 00:01:13 +0800

    [master] added the feature: randomly showing a word. 	modified:   main.py

--- calliope/main.py
+++ calliope/main.py
@@ -1,4 +1,9 @@
+import random
+
+
 if __name__ == '__main__':
-    vocabularies = open('data/vocabularies.txt', 'rb')
-    for vocabulary in vocabularies.readlines():
-        print vocabulary.strip()
+    vocabularies = open('data/vocabularies.txt', 'rb').readlines()
+    idx = random.randint(0, len(vocabularies))
+    print vocabularies[idx]
+    #for vocabulary in vocabularies.readlines():
+    #    print vocabulary.strip()
